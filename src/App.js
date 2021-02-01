1edd99b9f4d09e8ab1c3c5a4e287b8fa3747ad22
kh_karim <kharrazkarim1@gmail.com>
2021-02-01 22:55:44 +0100

    changed app text 2

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Change in the feature branch !!
-          </a>
+          karim
+        </a>
       </header>
     </div>
   );
